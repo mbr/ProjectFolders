ba90bcaae9d29f2004df93ce4b95219c862403b1
Marc Brinkmann <git@marcbrinkmann.de>
2015-08-10 10:38:07 +0200

    Added command quick panel.

--- ProjectFolders.py
+++ ProjectFolders.py
@@ -1,9 +1,32 @@
+import os
+
+import sublime
 import sublime_plugin
 
 
-class ProjectfolderCommand(sublime_plugin.ApplicationCommand):
+class ProjectfolderCommand(sublime_plugin.WindowCommand):
     def description(self):
         return 'Open a folder and the associated project'
 
+    def open_project_folder(self, path):
+        print('opening project associated with {}'.format(path))
+
     def run(self):
-        print('Hello, Sublime')
+        # collect list of packages
+        dirs = ['~']
+
+        entries = []
+
+        for basedir in dirs:
+            for dn in os.listdir(os.path.expanduser(basedir)):
+                entries.append(os.path.join(basedir, dn))
+
+        entries.sort()
+
+        def _open_entry(n):
+            if n == -1:
+                return
+
+            self.open_project_folder(entries[n])
+
+        self.window.show_quick_panel(entries, _open_entry)
