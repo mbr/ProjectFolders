7f1cfbfa09799e21bb84d7d2229985c708e8ac4e
Marc Brinkmann <git@marcbrinkmann.de>
2015-08-10 12:59:43 +0200

    Removed a few extra status messages.

--- ProjectFolders.py
+++ ProjectFolders.py
@@ -22,7 +22,6 @@ class ProjectfolderCommand(sublime_plugin.WindowCommand):
         return 'Open a folder and the associated project'
 
     def open_project_folder(self, path):
-        print('opening project associated with {}'.format(path))
         # information available for pathname generation
         d = {
             'basename': os.path.basename(path),
@@ -31,7 +30,6 @@ class ProjectfolderCommand(sublime_plugin.WindowCommand):
         }
 
         project_file = get_settings().get('project_file_format').format(d)
-        print('project file: {}'.format(project_file))
 
         # if the project does not exist, create it
         if not os.path.exists(project_file):
